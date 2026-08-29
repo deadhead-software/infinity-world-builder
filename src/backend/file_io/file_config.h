e3e1839513524cf3f7c45bafd7525f4c01ae2aac
PC\jason <jasoncmcqueen@gmail.com>
2026-08-29 19:44:13 -0400

    File I/O is exceptionally annoying. Even more annoying to test smh

--- src/backend/file_io/file_config.h
+++ src/backend/file_io/file_config.h
@@ -3,7 +3,7 @@
 * File Name         : file_config.h
 * Description       : currently a constants file, will be replaced later
 *                     with XML or TOML parsing, something like that
-*                    
+*
 * Revision History  :
 * Date		        Author 			Comments
 * ------------------------------------------------------------------
@@ -17,16 +17,26 @@
 
 namespace BE_CONSTANTS
 {
-    const std::filesystem::path dh_app_dir_name {"/Deadhead Software/"};
+    const std::filesystem::path dh_app_dir_name{"/Deadhead Software/"};
     const std::filesystem::path iwb_app_dir_name{"/Infinity World Builder"};
 
     namespace APPLE_CONST
     {
         /// @brief Horrible way to do this but I'm just trying to test things, I'll change how we do this later
-        const std::filesystem::path seed_db_asset_path {"/Users/root1/_SRC/infinity-world-builder/assets/db/seed/seed_db.db"};
-        const std::filesystem::path appdata_dir {"~/Library/Application Support"};
+        const std::filesystem::path seed_db_asset_path{"/Users/root1/_SRC/infinity-world-builder/assets/db/seed/seed_db.db"};
+        const std::filesystem::path appdata_dir{"~/Library/Application Support"};
         const std::filesystem::path iwb_appdata_dir = appdata_dir / dh_app_dir_name / iwb_app_dir_name; // concat operator
-        const std::filesystem::path userdata_dir { "~/Library/Preferences"};
-        const std::filesystem::path preferences { "/Preferences"};
+        const std::filesystem::path userdata_dir{"~/Library/Preferences"};
+        const std::filesystem::path preferences{"/Preferences"};
+    }
+
+    namespace WINDOWS_CONST
+    {
+        /// @brief Horrible way to do this but I'm just trying to test things, I'll change how we do this later
+        const std::filesystem::path seed_db_asset_path{"D:/_SRC/infinity-world-builder/assets/db/seed/seed_db.db"};
+        const std::filesystem::path appdata_dir{"~/Library/Application Support"};
+        const std::filesystem::path iwb_appdata_dir = appdata_dir / dh_app_dir_name / iwb_app_dir_name; // concat operator
+        const std::filesystem::path userdata_dir{"~/Library/Preferences"};
+        const std::filesystem::path preferences{"/Preferences"};
     }
 }
--- src/backend/file_io/file_io.cpp
+++ src/backend/file_io/file_io.cpp
@@ -8,12 +8,12 @@
 namespace FILE_IO
 {
 
-     bool file_io_main::initialize()
-     {
+    bool file_io_main::initialize()
+    {
         bool rc = initialize_module("file_io");
-#ifdef MACOS
+#ifdef APPLE
         logger->info("FILE I/O using MACOS directories");
-        if(!check_for_seed_db())
+        if (!check_for_seed_db())
         {
             return false;
         }
@@ -25,12 +25,24 @@ namespace FILE_IO
             }
         }
 #endif
-
+        logger->info("FILE I/O using WINDOWS directories");
+        if (!check_for_seed_db())
+        {
+            return false;
+        }
+        else
+        {
+            if (!handle_user_db())
+            {
+                logger->info("Couldn't create user db nor find it.");
+            }
+        }
         return true;
-     }
+    }
 
     bool file_io_main::check_for_seed_db()
     {
+#ifdef APPLE
         if (std::filesystem::exists(BE_CONSTANTS::APPLE_CONST::seed_db_asset_path))
         {
             logger->info("Seed DB exists!");
@@ -41,10 +53,24 @@ namespace FILE_IO
             logger->error("Seed DB does not exist :(");
             return false;
         }
+#endif
+#ifdef WINDOWS
+        if (std::filesystem::exists(BE_CONSTANTS::WINDOWS_CONST::seed_db_asset_path))
+        {
+            logger->info("Seed DB exists!");
+            return true;
+        }
+        else
+        {
+            logger->error("Seed DB does not exist :(");
+            return false;
+        }
+#endif
     }
 
     bool file_io_main::handle_user_db()
     {
+#ifdef APPLE
         if (std::filesystem::exists(BE_CONSTANTS::APPLE_CONST::iwb_appdata_dir))
         {
             logger->info("Deadhead Software/Infinity World Builder directory exists");
@@ -57,19 +83,20 @@ namespace FILE_IO
         else
         {
             logger->info("User IWB app directory doesn't exist.");
-            
+
             try
             {
                 sysdir_search_path_enumeration_state state = sysdir_start_search_path_enumeration(
                     SYSDIR_DIRECTORY_LIBRARY, // Directory type
-                    SYSDIR_DOMAIN_MASK_USER              // Domain mask (~/Library)
+                    SYSDIR_DOMAIN_MASK_USER   // Domain mask (~/Library)
                 );
 
                 char path_buffer[PATH_MAX];
                 std::error_code ec;
-                if (sysdir_get_next_search_path_enumeration(state, path_buffer)) {
-                    std::filesystem::path preferences_path { path_buffer };
-                    std::filesystem::path added_path { "Preferences"};
+                if (sysdir_get_next_search_path_enumeration(state, path_buffer))
+                {
+                    std::filesystem::path preferences_path{path_buffer};
+                    std::filesystem::path added_path{"Preferences"};
                     preferences_path = preferences_path / added_path;
                     logger->info("Got preferences path of: {}", preferences_path.c_str());
                     if (std::filesystem::exists(preferences_path / BE_CONSTANTS::dh_app_dir_name, ec))
@@ -89,13 +116,12 @@ namespace FILE_IO
                     }
                 }
             }
-            catch(const std::exception& e)
+            catch (const std::exception &e)
             {
                 std::cerr << e.what() << '\n';
             }
-            
+#endif
+#ifdef WINDOWS
+#endif
         }
     }
-
-
-}
