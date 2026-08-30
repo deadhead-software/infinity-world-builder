d5ae3315106c905e6437ab5ab870f9874dba25e4
PC\jason <jasoncmcqueen@gmail.com>
2026-08-29 20:17:21 -0400

    Actual hell, I'm in hell

--- src/backend/file_io/file_config.h
+++ src/backend/file_io/file_config.h
@@ -33,10 +33,8 @@ namespace BE_CONSTANTS
     namespace WINDOWS_CONST
     {
         /// @brief Horrible way to do this but I'm just trying to test things, I'll change how we do this later
-        const std::filesystem::path seed_db_asset_path{"D:/_SRC/infinity-world-builder/assets/db/seed/seed_db.db"};
-        const std::filesystem::path appdata_dir{"~/Library/Application Support"};
-        const std::filesystem::path iwb_appdata_dir = appdata_dir / dh_app_dir_name / iwb_app_dir_name; // concat operator
-        const std::filesystem::path userdata_dir{"~/Library/Preferences"};
-        const std::filesystem::path preferences{"/Preferences"};
+        const std::filesystem::path seed_db_asset_path{"./seed_db.db"};
+        /// @brief Add the appdata directory prior to this
+        const std::filesystem::path iwb_appdata_dir = dh_app_dir_name / iwb_app_dir_name; // concat operator
     }
 }
--- src/backend/file_io/file_io.cpp
+++ src/backend/file_io/file_io.cpp
@@ -1,8 +1,13 @@
 #include "file_io.h"
 #include "file_config.h"
 
-#ifdef MACOS
+#ifdef APPLE
 #include "sysdir.h"
+#endif
+#ifdef WINDOWS
+#include <windows.h>
+#include <shlobj.h>
+#include <iostream>
 #endif
 
 namespace FILE_IO
@@ -122,6 +127,68 @@ namespace FILE_IO
             }
 #endif
 #ifdef WINDOWS
+            if (std::filesystem::exists(BE_CONSTANTS::APPLE_CONST::iwb_appdata_dir))
+            {
+                logger->info("Deadhead Software/Infinity World Builder directory exists");
+                if (std::filesystem::exists(BE_CONSTANTS::APPLE_CONST::iwb_appdata_dir / "app_db.db"))
+                {
+                    logger->info("User Database exists");
+                }
+                return true;
+            }
+            else
+            {
+                logger->info("User IWB app directory doesn't exist.");
+
+                try
+                {
+                    PWSTR path = nullptr;
+                    std::filesystem::path preferences_path;
+                    // Use FOLDERID_RoamingAppData for Roaming, or FOLDERID_LocalAppData for Local
+                    HRESULT hr = SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &path);
+
+                    if (SUCCEEDED(hr))
+                    {
+                        // Convert PWSTR to a standard wide string or print it
+                        std::wstringstream ss;
+                        ss << path;
+                        std::wstring ss_str = ss.str();
+                        std::string str;
+                        std::transform(ss_str.begin(), ss_str.end(), std::back_inserter(str), [](wchar_t c)
+                                       { return (char)c; });
+                        logger->info("Found user's Local path: {}", str.c_str());
+                        preferences_path = path;
+                        // Remember to free the memory allocated by SHGetKnownFolderPath
+                        CoTaskMemFree(path);
+                    }
+                    else
+                    {
+                        logger->error("Failed to get user's appdata path");
+                    }
+                    std::filesystem::path app_path = preferences_path; // BE_CONSTANTS::dh_app_dir_name; /// BE_CONSTANTS::iwb_app_dir_name;
+                    std::error_code ec;
+                    std::string app_path_str = app_path.string();
+                    if (std::filesystem::exists(app_path, ec))
+                    {
+                        logger->info("{} user directory exists", app_path_str.c_str());
+                    }
+                    else
+                    {
+                        if (std::filesystem::create_directory(app_path, ec))
+                        {
+                            logger->info("Able to create directory: {}", app_path_str.c_str());
+                        }
+                        else
+                        {
+                            logger->info("failed to create DH/IWB user directory due to {}", ec.message());
+                        }
+                    }
+                }
+                catch (const std::exception &e)
+                {
+                    std::cerr << e.what() << '\n';
+                }
+            }
 #endif
         }
     }
